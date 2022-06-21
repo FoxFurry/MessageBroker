4434b4d8466b1ca5925b9568cbfbc24b8c3194b5
FoxFurry <isacartur@gmail.com>
2022-06-22 00:27:47 +0300

    Removed unnecessary stuff

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,7 +5,6 @@ import akka.actor.typed.ActorSystem
 object Main {
   def main(args: Array[String]): Unit = {
     val proxyGuardian: ActorSystem[ProxyAction] = ActorSystem(TopicProxy(), "proxy")
-
     val httpServer = new ServerHttp(proxyGuardian.log)(proxyGuardian)
     httpServer.Start()
   }
